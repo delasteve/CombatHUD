14ca51615ea655cc279d4f6045b79d1c80afeda4
Stephen Cavaliere <stephencavaliere@gmail.com>
2022-03-30 16:26:31 -0400

    testing events

---
+++ src/CombatHud/Logger.cs
@@ -0,0 +1,12 @@
+﻿using Decal.Adapter;
+
+namespace CombatHud
+{
+    internal class Logger
+    {
+        public void LogToChat(string message)
+        {
+            CoreManager.Current.Actions.AddChatText($"CombatHUD: {message}", 5);
+        }
+    }
+}
--- src/CombatHud/PluginCore.cs
+++ src/CombatHud/PluginCore.cs
@@ -1,4 +1,6 @@
-﻿using Decal.Adapter;
+﻿using System;
+using Decal.Adapter;
+using Decal.Adapter.Wrappers;
 
 namespace CombatHud
 {
@@ -6,12 +8,64 @@ namespace CombatHud
     [FriendlyName("CombatHUD")]
     public class PluginCore : PluginBase
     {
+        private Logger _logger;
+
         protected override void Startup()
         {
+            _logger = new Logger();
+
+            Core.WorldFilter.ChangeObject += WorldFilter_ChangeObject;
+            Core.WorldFilter.CreateObject += WorldFilter_CreateObject;
+            Core.WorldFilter.ReleaseObject += WorldFilter_ReleaseObject;
         }
 
         protected override void Shutdown()
         {
+        }
+        
+        [BaseEvent("LoginComplete", "CharacterFilter")]
+        private void CharacterFilter_LoginComplete(object sender, EventArgs e)
+        {
+            _logger.LogToChat("Initialized");
+
+        }
+
+        private void WorldFilter_CreateObject(object sender, CreateObjectEventArgs e)
+        {
+            if (IsNotMonsterObject(e.New.ObjectClass))
+            {
+                return;
+            }
+
+            var worldObjectCollection = Core.WorldFilter.GetByObjectClass(ObjectClass.Monster);
+            _logger.LogToChat($"New monster created. Total Count: {worldObjectCollection.Count}");
+        }
+
+        private void WorldFilter_ReleaseObject(object sender, ReleaseObjectEventArgs e)
+        {
+            if (IsNotMonsterObject(e.Released.ObjectClass))
+            {
+                return;
+            }
+
+            var worldObjectCollection = Core.WorldFilter.GetByObjectClass(ObjectClass.Monster);
+            _logger.LogToChat($"Monster deleted. Total Count: {worldObjectCollection.Count}");
+        }
+
+        private void WorldFilter_ChangeObject(object sender, ChangeObjectEventArgs e)
+        {
+            if (IsNotMonsterObject(e.Changed.ObjectClass))
+            {
+                return;
+            }
+
+            var worldObjectCollection = Core.WorldFilter.GetByObjectClass(ObjectClass.Monster);
+            _logger.LogToChat($"Monster changed. Total Count: {worldObjectCollection.Count}");
+        }
+
+        private bool IsNotMonsterObject(ObjectClass objectClass)
+        {
+            return objectClass != ObjectClass.Monster;
         }
     }
 }
